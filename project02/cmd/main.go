5543db24874a57c934b8d341a14c3278cba5ae36
Tanmay Mondal <tanmoy10122000@gmail.com>
2026-01-16 20:58:21 +0530

    ..

--- project02/cmd/main.go
+++ project02/cmd/main.go
@@ -1,11 +1,74 @@
 package main
 
-func main()  {
+import (
+	"log"
+	"os"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// app.go##########################################################
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.RealIP)    // rate lemiting
+	r.Use(middleware.RequestID) // rate lemeting and tressing
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer) // recover from crashes
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("working"))
+	})
+
+	// http.ListenAndServe(":8000", r)
+	return r
+}
+
+func(app *application) run(h http.Handler) error {
+srv :=&http.Server{
+	Addr: app.config.addr,
+Handler: h,
+WriteTimeout: time.Second*30,
+ReadTimeout: time.Second*10,
+IdleTimeout: time.Minute,
+}
+
+log.Println("server has started at", app.config.addr)
+
+return srv.ListenAndServe()
+}
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string
+}
+
+//main.go ##########################################################
+func main() {
 	cfg := config{
 		addr: ":8080",
-		db: dbConfig{},
+		db:   dbConfig{},
 	}
 	api := application{
 		config: cfg,
+	}
+
+	h := api.mount()
+	err := api.run(h)
+	if err != nil {
+		log.Println("server has faild to start err:",err)
+		os.Exit(1)
 	}
 }
